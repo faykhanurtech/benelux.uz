c420f8ffa24e6bad7d9cf52ca1f3a908f3e6d89a
Fayzullo <92411568+fayzullo1501@users.noreply.github.com>
2026-03-11 17:47:40 +0500

    add event section on home page

--- website/src/app/[locale]/page.tsx
+++ website/src/app/[locale]/page.tsx
@@ -1,8 +1,10 @@
 import Hero from "../../components/home/Hero"
+import Events from "../../components/home/Events"
 export default function HomePage() {
   return (
     <>
       <Hero />
+      <Events />
     </>
   )
 }
